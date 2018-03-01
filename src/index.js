1b068816165a3e4676cd78e9c4df4cba09710301
ShklyanikVladislav <36641082+ShklyanikVladislav@users.noreply.github.com>
2018-03-01 23:45:45 +0300

    task6/Fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
     var arr = str.split('');
-    var A = [];
-    for ( var i =0; i<=arr.length;i++){
-      if(arr[i]=='('||arr[i]=='['||arr[i]=='|'||arr[i]=='{'){
+    var A = []; 
+    st:
+    for ( var i =0; i<arr.length;i++) {
+      if(arr[i]=='('||arr[i]=='['||arr[i]=='{'){
         A.push(arr[i]);
+//continue st;
       }
-    }
-    for (var j = A.length; j<=arr.length; j++){
-      if(arr[j]==')'||arr[j]==']'||arr[j]=='|'||arr[j]=='}'){
-        A.pop(A.length-1);
-      }
-    }
+  
+  
+      //if(A.length==0&&arr.length!=(i+1)&&(arr[i]==')'||arr[i]==']'||arr[i]=='}')) return false;
+    if(arr[i]==')'&&A[A.length-1]=='(') A.pop(A.length-1); 
+
+    if(arr[i]=='}'&&A[A.length-1]=='{') A.pop(A.length-1);
+
+    if(arr[i]==']'&&A[A.length-1]=='[') A.pop(A.length-1);
+
+    if(arr[i]=='|'&&A[A.length-1]=='|') A.pop(A.length-1);
+
+    //if(arr[i]==arr[i-1]&&arr[i]=='|') A.pop(A.length-1);
+  }
+  
+       
     if(A.length==0) return true;
     else return false;
   }
-  
+
+
+//|(|)
