c44b0847d532db520a0b97bde68b0816e713ee91
ShklyanikVladislav <36641082+ShklyanikVladislav@users.noreply.github.com>
2018-03-01 18:51:54 +0300

    task_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var arr = str.split('');
+    var A = [];
+    for ( var i =0; i<=arr.length;i++){
+      if(arr[i]=='('||arr[i]=='['||arr[i]=='|'||arr[i]=='{'){
+        A.push(arr[i]);
+      }
+    }
+    for (var j = A.length; j<=arr.length; j++){
+      if(arr[j]==')'||arr[j]==']'||arr[j]=='|'||arr[j]=='}'){
+        A.pop(A.length-1);
+      }
+    }
+    if(A.length==0) return true;
+    else return false;
+  }
+  
